006e8104ee57bc7e45338b0fdd19b433baceb3ea
Steven Tendyra <steven.tendyra@postgrad.manchester.ac.uk>
2024-03-20 14:18:22 +0000

    split up functions to be BETTER

--- catchment/compute_data.py
+++ catchment/compute_data.py
@@ -8,6 +8,7 @@ import pandas as pd
 from catchment import models, views
 
 
+
 def analyse_data(data_dir):
     """Calculate the standard deviation by day between datasets.
 
@@ -20,11 +21,14 @@ def analyse_data(data_dir):
         raise ValueError('No CSV files found in the data directory')
     data = map(models.read_variable_from_csv, data_file_paths)
 
-    daily_std_list = []
-    for dataset in data:
-        daily_std = dataset.groupby(dataset.index.date).std()
-        daily_std_list.append(daily_std)
-    
-    daily_standard_deviation = pd.concat(daily_std_list)
+    daily_standard_deviation = compute_standard_deviation_by_day(data)
 
+    return daily_standard_deviation
+
+def daily_std(data):
+    return data.groupby(data.index.date).std()
+
+def compute_standard_deviation_by_day(data):
+    daily_std_list = map(daily_std, data)      
+    daily_standard_deviation = pd.concat(daily_std_list)
     return daily_standard_deviation
