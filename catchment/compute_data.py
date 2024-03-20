45aea772ebe4a772522db0e616c0a239999e9433
Ben <qq23840@bristol.ac.uk>
2024-03-20 14:32:36 +0000

    refactor compute_data.py and update docstrings

--- catchment/compute_data.py
+++ catchment/compute_data.py
@@ -14,17 +14,34 @@ def analyse_data(data_dir):
     Gets all the measurement data from the CSV files in the data directory,
     works out the standard deviation for each day. 
     """
+    data = load_catchment_data(data_dir)
+    daily_standard_deviation = compute_standard_deviation_by_day(data)
+    return daily_standard_deviation
+
+def compute_standard_deviation_by_day(data):
+    """
+    Calculate the standard deviation by day for a given list of datasets
+    Args:
+        data : data for which the standard deviations are to be calculated
+    Returns:
+        daily_standard_deviation : pandas Dataframe of standard deviations
+    """
+    daily_std_list = map(models.daily_std, data)
+    daily_standard_deviation = pd.concat(daily_std_list)
+    return daily_standard_deviation
+
+def load_catchment_data(data_dir):
+    """
+    Loads data from CSV files in the specified data directory
+    Args: 
+        data_dir : directory in which data files are located
+    Returns:
+        data : read-in data
+    Raises:
+        ValueError : If no .csv files are found in the target directory
+    """
     data_file_paths = glob.glob(os.path.join(data_dir, 'rain_data_2015*.csv'))
     if len(data_file_paths) == 0:
         raise ValueError('No CSV files found in the data directory')
     data = map(models.read_variable_from_csv, data_file_paths)
-    
-    daily_standard_deviation = compute_standard_deviation_by_day(data)
-
-    return daily_standard_deviation
-
-def compute_standard_deviation_by_day(data):
-    """Calculate the standard deviation by day for a given list of datasets"""
-    daily_std_list = map(models.daily_std, data)
-    daily_standard_deviation = pd.concat(daily_std_list)
-    return daily_standard_deviation
+    return data
