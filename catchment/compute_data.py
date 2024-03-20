5f12cd069df95ecd4e31996c508a2f02aacc316a
Pgordon <74874868+PriyaGordon@users.noreply.github.com>
2024-03-20 14:34:04 +0000

    compute_data.py refactor- further refinement

--- catchment/compute_data.py
+++ catchment/compute_data.py
@@ -10,16 +10,21 @@ from catchment import models, views
 def daily_std(data):
     return data.groupby(data.index.date).std()
 
+def load_catchment_data(data_dir):
+    data_file_paths = glob.glob(os.path.join(data_dir, 'rain_data_2015*.csv'))
+    if len(data_file_paths) == 0:
+        raise ValueError('No CSV files found in the data directory')
+    data = map(models.read_variable_from_csv, data_file_paths)
+    return data
+
+
 def analyse_data(data_dir):
     """Calculate the standard deviation by day between datasets.
     Gets all the measurement data from the CSV files in the data directory,
     works out the mean for each day, and then graphs the standard deviation
     of these means.
     """
-    data_file_paths = glob.glob(os.path.join(data_dir, 'rain_data_2015*.csv'))
-    if len(data_file_paths) == 0:
-        raise ValueError('No CSV files found in the data directory')
-    data = map(models.read_variable_from_csv, data_file_paths)
+    data = load_catchment_data(data_dir)
     daily_standard_deviation = compute_standard_deviation_by_day(data)
     return daily_standard_deviation
 
@@ -28,15 +33,3 @@ def compute_standard_deviation_by_day(data):
     daily_standard_deviation = pd.concat(daily_std_list)
     return daily_standard_deviation
 
-
-
-
-
-
-
-
-    # graph_data = {
-    #     'daily standard deviation': daily_standard_deviation
-    # }
-
-    # views.visualize(graph_data)
