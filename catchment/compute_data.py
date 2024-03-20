563880e47fcb80b285209ca5aa4d6260b5bddc2a
Ben <qq23840@bristol.ac.uk>
2024-03-20 14:18:34 +0000

    refactor and add tests for compute_data

--- catchment/compute_data.py
+++ catchment/compute_data.py
@@ -5,27 +5,26 @@ import glob
 import os
 import pandas as pd
 
-from catchment import models, views
+from catchment import models
 
 
 def analyse_data(data_dir):
     """Calculate the standard deviation by day between datasets.
 
     Gets all the measurement data from the CSV files in the data directory,
-    works out the mean for each day, and then graphs the standard deviation
-    of these means.
+    works out the standard deviation for each day. 
     """
     data_file_paths = glob.glob(os.path.join(data_dir, 'rain_data_2015*.csv'))
     if len(data_file_paths) == 0:
         raise ValueError('No CSV files found in the data directory')
     data = map(models.read_variable_from_csv, data_file_paths)
-
-    daily_std_list = []
-    for dataset in data:
-        daily_std = dataset.groupby(dataset.index.date).std()
-        daily_std_list.append(daily_std)
     
-    daily_standard_deviation = pd.concat(daily_std_list)
+    daily_standard_deviation = compute_standard_deviation_by_day(data)
 
     return daily_standard_deviation
 
+def compute_standard_deviation_by_day(data):
+    """Calculate the standard deviation by day for a given list of datasets"""
+    daily_std_list = map(models.daily_std, data)
+    daily_standard_deviation = pd.concat(daily_std_list)
+    return daily_standard_deviation
